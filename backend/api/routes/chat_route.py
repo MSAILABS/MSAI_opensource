8877475bf483de1e97c76c927760e7aebaa6d8ee
Rehan Shah <rehan.shah@talkontext.com>
2025-03-11 12:53:11 +0500

    records bug fix.

--- backend/api/routes/chat_route.py
+++ backend/api/routes/chat_route.py
@@ -3,17 +3,24 @@ import logging as log
 
 from fastapi import APIRouter, Request, status
 from fastapi.responses import JSONResponse
+from sqlalchemy.orm import Session
 
 from api.agents_api.chat_api import chat_with_agents
 from api.agents_api.schemas.chat_schema import Chat_Agent_Query
 from core.configurations import user_identifier
+from db.repository.records import get_all_records_from_db
 
 router = APIRouter()
 
 @router.post("/chat")
-async def chat_route(request: Request, data: Chat_Agent_Query):
+async def chat_route(request: Request, data: Chat_Agent_Query, db: Session = Depends(get_db)):
     try:
         data.identifier = user_identifier
+        records = get_all_records_from_db(db)
+
+        if len(records) < 1:
+            data.use_records = False
+
         response = await chat_with_agents(data)
 
         data = json.loads(response.text)
